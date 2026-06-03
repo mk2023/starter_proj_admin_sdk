7ee681952cb696b06fd0dad4e3f385764fb77d3e
Minseul Kim <minseulkim@google.com>
2026-06-03 14:00:44 -0700

    Fixed graphql query folder to have authentication levels

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,4 +12,4 @@ const app = initializeApp(firebaseConfig);
 export const dataConnectInstance = getDataConnect(app, connectorConfig);
 
 // Automatically connect to emulator on localhost:9399
-connectDataConnectEmulator(dataConnectInstance, 'localhost', 9399);
+connectDataConnectEmulator(dataConnectInstance, '127.0.0.1', 9399);
